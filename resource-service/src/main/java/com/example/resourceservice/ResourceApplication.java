36fd4cc65cc20e761cab72d2a499b7da479a2c2c
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2025-09-22 14:34:52 +0300

    Implement practical task for module 1 'Microservice architecture overview'

---
+++ resource-service/src/main/java/com/example/resourceservice/ResourceApplication.java
@@ -0,0 +1,14 @@
+package com.example.resourceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
+    }
+}
