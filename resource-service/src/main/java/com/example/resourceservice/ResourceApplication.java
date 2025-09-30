943a0c7fd0e2208341c6f52a4d16c495b5aa470a
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2025-09-30 15:01:08 +0300

    Implement practical task for module 3 'Service Discovery'

--- resource-service/src/main/java/com/example/resourceservice/ResourceApplication.java
+++ resource-service/src/main/java/com/example/resourceservice/ResourceApplication.java
@@ -2,11 +2,22 @@ package com.example.resourceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResourceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
