876c9c9280d05ff1ae9e52239af1ab2e47f40426
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2025-10-18 17:54:04 +0300

    openfeign

---
+++ resource-service/src/main/java/com/example/resourceservice/service/SongServiceClient2.java
@@ -0,0 +1,42 @@
+package com.example.resourceservice.service;
+
+import com.example.resourceservice.dto.CreateSongDto;
+import com.example.resourceservice.exception.SongAlreadyExistsException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SongServiceClient2 {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${song-service.url}")
+    private String songServiceUrl;
+
+    public void createSong(CreateSongDto createSongDto) {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        var requestEntity = new HttpEntity<>(createSongDto, headers);
+
+        try {
+            restTemplate.postForEntity(songServiceUrl + "/songs", requestEntity, Void.class);
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.CONFLICT) {
+                throw new SongAlreadyExistsException(createSongDto.id());
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    public void deleteSong(Long id) {
+        restTemplate.delete(songServiceUrl + "/songs?id=" + id);
+    }
+}
